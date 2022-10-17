a38b4d9ebde262caf497ae0431c1bfca071dd9b4
JumpShipper <115733574+JumpShipper@users.noreply.github.com>
2022-10-16 19:39:31 -0700

    Add files via upload

---
+++ Dynamic_Array.py
@@ -0,0 +1,37 @@
+# import ctypes
+#
+#
+# class DynamicArray(object):
+#     def __init__(self):
+#         self.size = 0
+#         self.capacity = 1
+#         self.Arr = self.make_array(self.capacity)
+#
+#     def __len__(self):
+#
+#
+#     def __getitem__(self, k):
+#
+#
+#     def append(self, element):
+#
+#
+#     def insertAt(self, item, index):
+#
+#
+#     def delete(self):
+#
+#
+#     def removeAt(self,idx):
+#
+#
+#     def _resize(self, new_cap):
+#
+#
+#     def make_array(self, new_cap):
+#
+#
+#     def print_arr(self):
+#
+#
+#     def insertList(self, inserting_list):
---
+++ main.py
@@ -0,0 +1,31 @@
+from Dynamic_Array_Completed import *
+from Dynamic_Array import *
+
+print("--------------------Dynamic Array (dynamicArray.py)--------------------")
+print("\n\n")
+#create a new dynamic array
+arr = DynamicArray()
+#append some numbers
+arr.append(1)
+arr.append(2)
+arr.append(2)
+arr.append(3)
+arr.append(4)
+arr.append(5)
+#print dynamic array
+arr.print_arr()
+#delete method
+arr.delete()
+arr.print_arr()
+#insert Method
+arr.insertAt(5,1)
+arr.print_arr()
+#removeAt
+arr.removeAt(1)
+arr.print_arr()
+# List to dynamic array
+arr2 = DynamicArray()
+listA = [1,2,3,4,5,10]
+arr2.insertList(listA)
+arr2.print_arr()
+
